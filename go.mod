2ab82d03165ac36a01a03cf757689eaa254bf9a9
Alexandre <alexandre.suriano@gmail.com>
2021-09-10 04:10:17 -0300

    add writer func

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexsuriano/kafkaGoStudy
+
+go 1.16
+
+require github.com/segmentio/kafka-go v0.4.18
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	writer := &kafka.Writer{
+		Addr:  kafka.TCP("localhost:9092"),
+		Topic: "info-topic",
+	}
+
+	err := writer.WriteMessages(context.Background(), kafka.Message{
+		Value: []byte("mensagem"),
+	})
+
+	if err != nil {
+		log.Fatal("cannot write a message:", err)
+	}
 }
